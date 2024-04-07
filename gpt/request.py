ced5c70ebdfdf33982e5017caba2600976fbae88
yaosheng <yaosheng@luckybus.me>
2024-04-07 18:18:21 +0800

    -edit:更新调用GPT Demo

--- gpt/request.py
+++ gpt/request.py
@@ -1,29 +1,31 @@
 import openai
 from flask import Flask
+import os
 
 # 实例化Flask
 app = Flask(__name__)
+
+os.environ['OPENAI_KEY'] = 'sk-xMC8z5QZoN9GdrUp724a2fBdC3C44aE68e51D2732377E2Be'
+os.environ['OPENAI_API_BASE'] = 'https://ai-yyds.com/v1'
+
+openai.api_key = os.getenv('OPENAI_KEY')
+openai.api_base = os.getenv("OPENAI_API_BASE")
 
 
 @app.route('/gpt/search/<message>', methods=['GET'])
 def gptService(message):
     print('message is:', message)
     # 调用openai的API
-    client = openai.OpenAI(api_key='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')
-    chat_completion = client.chat.completions.create(
-        # 发送的消息内容
-        messages=[
-            {"role": "user", "content": message},
-        ],
-        # 选择模型(模型可以在openai官网查到)
-        model='gpt-3.5-turbo-1106',
-        # 结果生成结束以后，同步返回
+    messages = [{"role": "user", "content": message}]
+    res = openai.ChatCompletion.create(
+        model="gpt-4-1106-preview",
+        messages=messages,
         stream=False,
         timeout=3000
     )
     # 结果
-    print('ChatGPT result is:', chat_completion.choices[0].message)
-    return chat_completion.choices[0].message
+    print('GPT结果:' + res['choices'][0]['message']['content'])
+    return res['choices'][0]['message']['content']
 
 
 if __name__ == '__main__':
